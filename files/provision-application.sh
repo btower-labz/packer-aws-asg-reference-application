d20f9c625d191a61a2067536f27763f51823eada
Roman Syrtsev <rvs@btower.net>
2020-09-13 20:59:36 +0000

    added cleanup, fixed app provisioning

--- files/provision-application.sh
+++ files/provision-application.sh
@@ -15,19 +15,29 @@ function log {
 
 log 'Started ...'
 
-
 export APP_URL=https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war
 export APP_SRC=/usr/local/src/application
 export APP_DST=/opt/wildfly/standalone/deployments
 
+log 'Variables configured ...'
+
 mkdir -p ${APP_SRC}
-curl --silent --location -o ${APP_SRC}/sample.war
+curl --silent --location -o ${APP_SRC}/sample.war ${APP_URL}
+ls -la ${APP_SRC}/sample.war
+
+log 'Application downloaded ...'
 
 # TODO: validate\checksum archive
 
 cp ${APP_SRC}/sample.war ${APP_DST}/service1.war
 cp ${APP_SRC}/sample.war ${APP_DST}/service2.war
 cp ${APP_SRC}/sample.war ${APP_DST}/service3.war
+ls -la ${APP_DST}
+
+log 'Application installed ...'
+
+cd /tmp
+rm -rf ${APP_SRC}
 
 # TODO: validate packages with wildfly
 
